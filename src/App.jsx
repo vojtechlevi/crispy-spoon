59bd864cf3cb1929b3f33195c7ceb3bf725ac29b
Levi Ekström <vojtechlevi1@hotmail.com>
2024-04-22 14:19:27 +0200

    topten and terrorists

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import Wanted from "./components/Wanted";
+import Wanted from "./pages/Wanted";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/wanted" element={<Wanted />} />
+      <Route
+        path="/topten"
+        element={
+          <Wanted
+            url={
+              "https://api.fbi.gov/@wanted?pageSize=20&page=1&sort_on=modified&sort_order=asc&poster_classification=ten"
+            }
+          />
+        }
+      />
+      <Route
+        path="/terrorist"
+        element={
+          <Wanted
+            url={
+              "https://api.fbi.gov/@wanted?pageSize=20&page=1&sort_on=modified&sort_order=asc&poster_classification=terrorist"
+            }
+          />
+        }
+      />
     </Routes>
   );
 }
